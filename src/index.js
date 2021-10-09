31e8d0efb622436dc7614520e05dbf0c16a10d17
OLEG <schastliviy.sluchay@ukr.net>
2021-10-09 16:06:13 +0300

    Log_in_mobile
    Сделал меню логин для мобилки и повесил слушатели

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import './sass/main.scss';
 
 
 // Переменные________________
+const loginButton = document.querySelector('#log_in_btn');
+const loginWindow = document.querySelector('.login__section');
+const loginClose = document.querySelector('#login__close_btn');
+
+
+
 
 
 
@@ -12,7 +18,8 @@ import './sass/main.scss';
 
 
 // Слушатели событий_________
-
+loginButton.addEventListener('click', () => { loginWindow.classList.remove('visually-hidden') });
+loginClose.addEventListener('click', () => { loginWindow.classList.add('visually-hidden') });
 
 
 
