78a8d7c00b135867dd8151221ae0260e89226293
Leila-2 <liliya.yavorskaya17@gmail.com>
2021-10-09 23:17:14 +0300

    Все работает)

--- src/index.js
+++ src/index.js
@@ -32,6 +32,7 @@ loginAcceptButton.addEventListener('click', () => { sendLogin(nameValue, input, 
 
 //Остальной код_______________
 
+
 const API_KEY = '?api_key=61165aac189ece3ae64e67d82e58db65';
 const BASE_URL = 'https://api.themoviedb.org/3/';
 let params = ''
