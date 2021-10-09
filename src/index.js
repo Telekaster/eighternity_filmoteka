28609180d33b4418026a211d6ffca5917897fa2f
Leila-2 <liliya.yavorskaya17@gmail.com>
2021-10-09 23:14:09 +0300

    Делает фетч по трендам🥳🥳🥳

--- src/index.js
+++ src/index.js
@@ -16,9 +16,10 @@ const loginAcceptButton = document.querySelector('.login__accept_btn');
 const input = document.querySelector('.login__input');
 const greetingText = document.querySelector('.login__greetings');
 let nameValue = '';
+const list = document.querySelector('.movies')
 
 const user = {
-    name:'',
+    name: '',
 }
 
 
@@ -30,4 +31,36 @@ loginAcceptButton.addEventListener('click', () => { sendLogin(nameValue, input, 
 
 
 //Остальной код_______________
+
+const API_KEY = '?api_key=61165aac189ece3ae64e67d82e58db65';
+const BASE_URL = 'https://api.themoviedb.org/3/';
+let params = ''
+
+
+params = 'trending/all/day'
+
+let url = BASE_URL + params + API_KEY;
+fetch(url)
+    .then(resp => resp.json())
+    .then(data => data.results)
+    .then(array => {
+        let result = array.map(elem => {
+            console.log('Result:', elem)
+            const { poster_path, backdrop_path, name, vote_average, first_air_date, title, id } = elem
+
+            return `
+            <li class="movies__item">
+            <div class="movie__card">
+            <img class="movie__img" src="https://image.tmdb.org/t/p/w500/${poster_path}" loading="lazy" alt="" data-src = "https://image.tmdb.org/t/p/w500/${backdrop_path}"/>
+            
+            <div class="movie__label">
+            <h3 class="movie__name">${title || name}</h3>
+             <p class="movie__genre">Жанр<span class="movie__year">${first_air_date || '2021'}</span></p>
+            </div>
+            </div>
+            </li>`
+        }).join('')
+        list.insertAdjacentHTML('beforeend', result)
+
+    })
 
