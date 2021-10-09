fac5658823c403e3fa881ec0fc0460ffbd6752b0
OLEG <schastliviy.sluchay@ukr.net>
2021-10-09 22:41:37 +0300

    Login 1.4
    Кнопка Accept теперь срабатывает на ENTER

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import loginWindowOpen from './js/login_window';
 import closeLoginWindow from './js/login_close_window';
 import takeValue from './js/login_takeValue';
 import sendLogin from './js/login_sendLogin';
+import submitByEnter from './js/login_submit_by_enter';
 
 
 // Переменные________________
@@ -16,12 +17,9 @@ const loginAcceptButton = document.querySelector('.login__accept_btn');
 const loginInput = document.querySelector('.login__input');
 const greetingText = document.querySelector('.login__greetings');
 let nameValue = '';
-
 const user = {
     name:'',
 }
-
-
 
 
 // Слушатели событий_________
@@ -29,26 +27,7 @@ loginButton.addEventListener('click', () => { loginWindowOpen(search, loginWindo
 loginClose.addEventListener('click', () => { closeLoginWindow(search, loginWindow) });
 loginInput.addEventListener('input', (evt) => { return nameValue = takeValue(evt.target.value, loginAcceptButton) });
 loginAcceptButton.addEventListener('click', () => { sendLogin(nameValue, loginInput, user, greetingText, loginWindow, search) });
-
-
-// document.addEventListener('keyup', ((evt) => {
-//     event.preventDefault()
-//     if (loginAcceptButton.hasAttribute('desabled')) {
-//         console.log('1');
-//         if (evt.key === 'Enter') {
-//                 // console.log(evt);
-//         console.log('2');
-//         sendLogin(nameValue, loginInput, user, greetingText, loginWindow, search)
-//         };
-//     };
-
-
-
-
-
-
-// }));
-
+window.addEventListener('keydown', (evt) => { submitByEnter(loginAcceptButton, evt, nameValue, loginInput, user, greetingText, loginWindow, search) });
 
 
 //Остальной код_______________
