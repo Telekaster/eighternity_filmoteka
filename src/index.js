0a642817d6ad3455e242962fde4c6180e1f84aa3
OLEG <schastliviy.sluchay@ukr.net>
2021-10-11 13:14:26 +0300

    fix index.js 1.0
    1. Перенес вынес весь код из index.js
    2. Вынес селекторы в refs

--- src/index.js
+++ src/index.js
@@ -2,36 +2,6 @@
 import './sass/main.scss';
 import './js/open-library';
 import './js/footer';
-import refs from './js/refs';
-import loginWindowOpen from './js/login_window';
-import closeLoginWindow from './js/login_close_window';
-import takeValue from './js/login_takeValue';
-import sendLogin from './js/login_sendLogin';
-import submitByEnter from './js/login_submit_by_enter';
-import API from './js/api.js';
+import './js/login';
+import './js/api.js';
 import './js/modal.js';
-// Переменные________________
-
-const { loginButton, search, loginWindow, loginClose, loginAcceptButton, loginInput, greetingText } = refs();
-
-
-let nameValue = '';
-
-// Добавила Лиля________________
-const list = document.querySelector('.movies')
-// _____________________________
-
-const user = {
-    name: '',
-}
-
-
-// Слушатели событий_________
-loginButton.addEventListener('click', () => { loginWindowOpen(search, loginWindow, loginAcceptButton) });
-loginClose.addEventListener('click', () => { closeLoginWindow(search, loginWindow) });
-loginInput.addEventListener('input', (evt) => { return nameValue = takeValue(evt.target.value, loginAcceptButton) });
-loginAcceptButton.addEventListener('click', () => { sendLogin(nameValue, loginInput, user, greetingText, loginWindow, search) });
-window.addEventListener('keydown', (evt) => { submitByEnter(loginAcceptButton, evt, nameValue, loginInput, user, greetingText, loginWindow, search) });
-
-
-//Остальной код_______________
--- src/js/refs.js
+++ src/js/refs.js
@@ -1,11 +1,25 @@
 export default function refs () {
     return {
+        // Олег_______________________________________________
         loginButton: document.querySelector('.log_in__button'),
         search: document.querySelector('.search'),
         loginWindow: document.querySelector('.login__section'),
         loginClose: document.querySelector('.login__close_btn'),
         loginAcceptButton: document.querySelector('.login__accept_btn'),
         loginInput: document.querySelector('.login__input'),
-        greetingText: document.querySelector('.login__greetings')
+        greetingText: document.querySelector('.login__greetings'),
+        // Лиля_________________________________________________
+        list: document.querySelector('.movies'),
+        // Марина_______________________________________________
+        // элементы хедера:
+        headerMain: document.getElementById('headerMain'),
+        headerLib: document.getElementById('headerLib'),
+        btnHomeOpen: document.getElementById('btn-home-open'),
+        btnLibOpen: document.getElementById('btn-lib-open'),
+        // кнопки в бібліотеці:
+        watchedBtn: document.getElementById('watched__btn'),
+        queueBtn: document.getElementById('queue__btn'),
+        //(хто буде підключати, то кнопки мають стилі на активний стан, властивість current)
+        
     };
 };
