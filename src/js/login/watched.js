63f4dd0e3a04678dc9fdb00b73976b1a5b423e23
OLEG <schastliviy.sluchay@ukr.net>
2021-10-11 17:06:42 +0300

    login-fix
    Пофиксил новый пользователь с пустым массивом

--- src/js/login/watched.js
+++ src/js/login/watched.js
@@ -2,7 +2,7 @@ export default function writeWatched (user) {
     document.addEventListener('click', ((event) => {
     if (event.target.className === 'modal-btn btn-watched') {
         user.watched.push(Number(event.target.getAttribute('id')));
-        console.log(user);
+        // console.log(user);
 
         localStorage.setItem(user.name, JSON.stringify(user));
     };
